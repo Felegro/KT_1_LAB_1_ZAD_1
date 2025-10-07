17b9124dac9b3f67367e9a11d20124f0d3caf946
Felegro <154550219+Felegro@users.noreply.github.com>
2025-10-07 17:18:06 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+[<EntryPoint>]
+let main _ =
+    // Спросим сначала, сколько чисел вводить
+    printf "Сколько чисел будете вводить? "
+    let n = Console.ReadLine() |> int
+
+    let modules =
+        [
+            let getAbs x = if x < 0 then -x 
+                            else x
+            for _ in 1 .. n do
+                let num = Console.ReadLine() |> int
+                yield getAbs num
+        ]
+
+    printfn "Модули введённых чисел: %A" modules
+    0
